79a68e679fe27e48b5984300690950b7058baa4f
Marshal10 <marshalmaleane123@gmail.com>
2024-06-17 15:54:27 +0530

    markup and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 export default function App() {
   return (
     <div className="app">
-      <h1>Hello React</h1>
+      <button>Get my position</button>
+      <p>
+        Your GPS position :{" "}
+        <a target="_blank" rel="noreferrer" href="#">
+          lat,lng
+        </a>
+      </p>
+      <p>You requested position 0 times</p>
     </div>
   );
 }
