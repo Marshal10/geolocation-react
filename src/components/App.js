ac6d6d96adede918939096e98c7a3b7f206df3a6
Marshal10 <marshalmaleane123@gmail.com>
2024-06-20 21:55:30 +0530

    Separation of components

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,5 @@
 import { useState } from "react";
-
-function useGeolocation() {
-  const [position, setPosition] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  function getPosition() {
-    if (!navigator.geolocation) {
-      return setError("Your browser does not support geolocation");
-    }
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
-  }
-
-  return { position, isLoading, error, getPosition };
-}
+import useGeolocation from "./useGeolocation";
 
 export default function App() {
   const [countClicks, setCountClicks] = useState(0);
